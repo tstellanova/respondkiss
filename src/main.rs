f7da64db5a0cb7e628d400f2ae1477a88de8ebd0
Todd Stellanova <tstellanova@users.noreply.github.com>
2024-05-11 11:57:17 -0700

    wip with addressee in msg field

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,98 @@ use std::fs;
 use std::path::Path;
 use std::io::{BufWriter, Write};
 use notify::{Watcher, RecommendedWatcher, RecursiveMode, Result, Config, event::{EventKind, AccessKind, AccessMode} };
-use chrono::{Local};
+use chrono::{DateTime, Local, TimeDelta, Timelike};
+use std::sync::Mutex;
+
+const MY_CALL:&'static str= "KM6NLE";
+const MY_CALL_EXT:&'static str= "KM6NLE-6";
+
+static LAST_BROADCAST: Mutex<Option<DateTime<Local>>> = Mutex::new(None);
+
+const BROADCAST_TIMEOUT:TimeDelta= TimeDelta::seconds(90);  
 
 fn handle_msgs(hay: &str, out_path: &Path) -> Result<() > {
+
   //let re = Regex::new(r"\[([0-9]+)\].([a-zA-Z0-9-_]+)>([a-zA-Z0-9-_]+),([a-zA-Z0-9-_\*]+):").unwrap();
   let re = Regex::new(r"\[([0-9]+)\].([a-zA-Z0-9-_]+)>([a-zA-Z0-9-_]+)").unwrap();
+  let addressee_re = Regex::new(r"::([ a-zA-Z0-9-\s]{9}):").unwrap();
 
+  // sample messages from KISS inbox:
+  // [0] N6RVI-6>APTIGR,RS0ISS*:ARISS Ground Monitoring Station  DM12lr
+  // [0] W0SX-10>CQ,RS0ISS*:=3701.15N/11332.30W-Hello from Utah! DM37 w0sx@arrl.org {UISS54}
 
   let mut results = vec![];
   for (_, [chan_num, origin, dest ] ) in re.captures_iter(&hay).map(|c| c.extract()) {
       results.push(( chan_num, origin, dest ));
   }
+  let first_msg = results.get(0); 
 
-  println!("Message Count: {}", results.len());
-  for result in results {
-    //println!("{:?}",result);
-    let new_dest = result.1;
-    // skip replying to ISS
-    if new_dest.contains("ISS") {
-      continue;
+  let mut addr_results = vec![];
+  for (_, [whole_addressee, inner_addressee ] ) in addressee_re.captures_iter(&hay).map(|c| c.extract()) {
+      addr_results.push(( whole_addressee, inner_addressee));
+  }
+  let first_addressee = addr_results.get(0);
+  let now = Local::now();
+
+
+  let mut broadcast_time_opt = *LAST_BROADCAST.lock().unwrap();
+  if let Some(broadcast_time) = broadcast_time_opt {
+    let elapsed = now.signed_duration_since(broadcast_time);
+    if elapsed > BROADCAST_TIMEOUT {
+      broadcast_time_opt = None;
     }
+   }
 
-    // [0] N6RVI-6>APTIGR,RS0ISS*:ARISS Ground Monitoring Station  DM12lr
-    // [0] W0SX-10>CQ,RS0ISS*:=3701.15N/11332.30W-Hello from Utah! DM37 w0sx@arrl.org {UISS54}
-    let new_msg = format!("KM6NLE-10>{},ARISS:=3752.42N/12217.42W-Roger CM87uu",new_dest);
-    println!("out: {}", new_msg);
-    let now = Local::now();
+  if let Some(in_header) = first_msg {
+    let origin = in_header.1;
+    let dest = in_header.2;
+ 
+    let response =  
+      if dest.contains(MY_CALL) {
+        Some(format!("{}>{},ARISS:=3752.42N/12217.42W(QSL 73s", MY_CALL_EXT, origin))
+      }
+      else if dest.eq("ALL") {
+        Some(format!("{}>{},ARISS::{:<9}: Heard CM87uu",MY_CALL_EXT, origin, origin))
+      }
+      else if dest.eq("CQ") ||
+	dest.starts_with("AP") { // Catch all APRS clients -- sorry, Pakistan
+        if let Some(addressee) = first_addressee {
+	  println!("RK addressee: {:?}", addressee);
+          if addressee.1.contains(&MY_CALL) {
+            Some(format!("{}>CQ,ARISS::{:<9}: QSL 73s ",MY_CALL_EXT, origin))
+          }
+          else {
+            // destination is CQ but theres an addressee who's not us
+            // Don't respond
+	    None
+          }  
+        }
+        else {
+          // acknowledge heard 
+          Some(format!("{}>CQ,ARISS::{:<9}: Heard CM87uu",MY_CALL_EXT, origin))
+        }
+      }
+      else {
+        if broadcast_time_opt.is_none() {
+          // track the last 
+          broadcast_time_opt = Some(now);
+          Some(format!("{}>CQ,ARISS:=3752.42N/12217.42W(Hello ARISS {:02}:{:02}:{:02}", 
+		MY_CALL_EXT, now.hour(), now.minute(), now.second()))
+        }
+        else {
+          None
+	}
+      };
+
+    *LAST_BROADCAST.lock().unwrap() = broadcast_time_opt;
+
+    if response.is_none() {
+      return Ok(())
+    } 
+    let new_msg = response.unwrap();
     let now_str = now.to_rfc3339();
+    println!("RK {} out: {}", now_str, new_msg);
+
     let full_out_path = out_path.join(now_str);
     //println!("full_out_path: {:?}",full_out_path);
     let out_file = fs::File::create(full_out_path)?;
@@ -42,7 +107,7 @@ fn handle_msgs(hay: &str, out_path: &Path) -> Result<() > {
   Ok(())
 }
 
-fn monitor_msgs<P: AsRef<Path> >(in_path: P, out_path:&Path ) -> notify::Result<()> {
+fn monitor_msgs<P: AsRef<Path> >(in_path: P, out_path:&Path, _save_path:&Path ) -> notify::Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
 
     // Automatically select the best implementation for your platform.
@@ -63,12 +128,14 @@ fn monitor_msgs<P: AsRef<Path> >(in_path: P, out_path:&Path ) -> notify::Result<
                 //println!("{:?}",event.paths[0]);
                 let in_msg_path = event.paths[0].clone();
                 let hay = fs::read_to_string(&in_msg_path)?;
-                println!("in: {:?}", hay);
+                let now = Local::now();
+    		let now_str = now.to_rfc3339();
+                println!("RK {} in: {:?}", now_str, hay);
                 handle_msgs(&hay, &out_path)?;
                 fs::remove_file(&in_msg_path)?;
               } 
             },
-            Err(e) => println!("watch error: {:?}", e),
+            Err(e) => println!("RK watch error: {:?}", e),
         }
     }
 
@@ -79,15 +146,16 @@ fn monitor_msgs<P: AsRef<Path> >(in_path: P, out_path:&Path ) -> notify::Result<
 fn main() -> Result< ()>  {
   let args: Vec<String> = env::args().collect();
 
-  println!("start! with: {:?}", args);
-  if args.len() < 3 {
-   println!("usage: respondkiss in_path out_path ");
+  println!("RK {} start! with:\r\n {:?}", Local::now(), args);
+  if args.len() < 4 {
+   println!("RK usage: respondkiss in_path out_path save_path ");
    return Ok(());
   }
   
   let in_path = Path::new(&args[1]);
   let out_path = Path::new(&args[2]); 
-  monitor_msgs(in_path, out_path )?;
+  let save_path = Path::new(&args[3]);
+  monitor_msgs(in_path, out_path, save_path )?;
 
   Ok(())
 }
